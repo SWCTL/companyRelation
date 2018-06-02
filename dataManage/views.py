d078d53d47e8735009f4dcadd4b4dcc87ad9384c
anwel <201692095@mail.dlut.edu.cn>
2018-06-02 09:45:44 +0800

    company

--- dataManage/views.py
+++ dataManage/views.py
@@ -7,7 +7,6 @@ from dataManage import models
 def index(request):
     return render(request, "index.html")
 
-
 def result(request):
     return render(request, "result.html")
 
@@ -17,10 +16,19 @@ def fail(request):
 def search(request):
     com_name = request.GET.get('com_name')
     error_msg = ''
-    company = models.TCorp.objects.get(corp_name=com_name)
 
     if not com_name:
         error_msg = '请输入关键词'
         return render(request, 'fail.html', {'error_msg': error_msg})
 
+    company = models.TCorp.objects.get(corp_name=com_name)
+
+
+
     return render(request, "result.html", {'company': company})
+
+def ownershipStucture(company):
+    id = company.id
+    org = company.org
+    seqId = company.seq_id
+    shareholderList = models.TMCorpCorpDist.objects.get(id=id, org=org, seq_id=seqId)
